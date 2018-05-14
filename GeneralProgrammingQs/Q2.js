1cfed279a75a46e9d7ffbb5690bbb2291382c520
HamadaFMahdi <hamadamahdi2014@gmail.com>
2018-05-14 20:28:54 +0100

    Q3 done

---
+++ GeneralProgrammingQs/Q2.js
@@ -0,0 +1,19 @@
+// I have not studied computer Science formula and I have only been coding (properly)
+// for maybe a year or so
+
+
+// I normally use Objects to handle that in JS like this:
+
+const laptops = 
+    {
+        "Apple":"Macbook Pro",
+        "Microsoft":"Surface Pro",
+        "Dell":"XPS"
+    }
+
+console.log(laptops.Apple);
+
+
+// However, I am very keen on leanring more computer science
+// knowledge and strengthening my fundementals.
+
---
+++ GeneralProgrammingQs/Q3.js
@@ -0,0 +1,20 @@
+// // I will assume both lists are sorted in ASCENDING order
+// // as per the example given
+
+// // I will also assume that the length of the final array must 
+// // always be the same as the sum of the two initial arrays
+// // so [1,2,3,4] & [3,4,5] => [1,2,3,3,4,4,5] and NOT [1,2,3,4,5]
+
+function mergeArrays (arr1, arr2) { 
+    arr1.push(...arr2);
+    mergedArray = arr1.sort((x,y) => x-y);
+
+    return mergedArray
+}
+
+console.log(mergeArrays([],[]));
+console.log(mergeArrays([1,4,6,9,11,45],[7,7,7,8,8,10]));
+console.log(mergeArrays([11,1,9,11,45],[7,7,7,8,8,10]));
+
+
+
